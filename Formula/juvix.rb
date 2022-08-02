0ed701912643f66912177af1c176cd1c55c258ec
Paola Lizarralde <dlizarra@eafit.edu.co>
2022-08-03 01:17:11 +0200

    Update juvix.rb

--- Formula/juvix.rb
+++ Formula/juvix.rb
@@ -5,8 +5,8 @@ class Juvix < Formula
   sha256 "6c4cff2655c2f13d0770c40e5473b03f25cc049edc73b4f3c4458013032f70ec"
   license "GPL-3.0"
   
-  resource("juvix-stdlib") do
-     url "https://github.com/anoma/juvix-stdlib.git"
+  resource("stdlib") do
+     url "https://github.com/anoma/juvix-stdlib.git", branch: "main"
   end
   
   head do
@@ -21,8 +21,7 @@ class Juvix < Formula
   depends_on "d12frosted/emacs-plus/emacs-plus@28" => :optional
   
   def install
-    resource("juvix-stdlib").stage do
-      system "rm", "-f", "juvix-stdlib"
+    resource("stdlib").stage do
       system "make", "install"
       prefix.install "README.org"
       prefix.install "LICENSE" 
